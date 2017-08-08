6e24393670c421dcb42e60d1c20e9b637dff3760
Jorick van Hees <jorick@Joricks-MacBook-Pro.local>
2017-08-08 14:29:10 +0200

    Fixed spam protection to be similair as dota 2 chat wheel sounds

--- soundboard/server/src/room.js
+++ soundboard/server/src/room.js
@@ -5,65 +5,74 @@ var fs = require('fs');
 
 var Sounds = require('../../src/sounds.js');
 
-var spamPoints = {};
+var spamTimeout = 120;
+var maxSounds = 2;
 
-function Room(client, roomId){
-  this.clients = [];
-  this.uuid = roomId ? roomId : String(uuid.v4());
-  this.addClient(client);
-  this.player = require('play-sound')(opts = {})
-  this.soundsFolder = path.resolve(appRoot, '..', 'public', 'sounds');
+function formatTime(value) {
+    return Math.floor(value / 60) + ":" + (value % 60 ? value % 60 : '00');
 }
 
-setInterval(function() {
-	Object.keys(spamPoints).forEach(function(key,index) {
-		if(spamPoints[key] > 0) spamPoints[key]--;
-	});
-	console.log(spamPoints);
-}, 1000 * 60);
+function Room(client, roomId){
+	this.clients = [];
+	this.uuid = roomId ? roomId : String(uuid.v4());
+	this.addClient(client);
+	this.player = require('play-sound')(opts = {})
+	this.soundsFolder = path.resolve(appRoot, '..', 'public', 'sounds');
+}
 
 Room.prototype = {
-  play: function(sound, sourceClient){
-	var address = sourceClient.connection._socket.remoteAddress;
-	if(typeof spamPoints[address] == 'undefined'){
-		spamPoints[address] = 0;
-	}
-	spamPoints[address]++;
-	// Cheap antispam
-	if(spamPoints[address] > 3){
-		console.log('Antispam enabled...');
-		return;
-	}
-	
-    console.log("playing ", sound.name, "in room", this.uuid);
-    var soundFile = path.resolve(this.soundsFolder, sound.file);
 
-    if(this.uuid == 'server'){
-      // Play file on the server
-      if (fs.statSync(soundFile) && this.player){
-        this.player.play(soundFile, function(err){
-          if (err)
-            console.log('Couldn\'t play file "' + soundFile + '"');
-        });
-      }
-    }
-    // Send command to clients to play sound
-    this.clients.forEach(function(client){
-      if (client.uuid != sourceClient.uuid)
-        client.send(this.playSoundCommand(sound));
-    }.bind(this));
-  },
-  playSoundCommand: function(sound){
-    return JSON.stringify({"command": "play", "sound": sound});
-  },
-  addClient: function(client){
-    this.clients.push(client);
-    client.currentRoom = this;
-  },
-  removeClient: function(client){
-    this.clients = this.clients.filter(function(c) { return c != client });
-    client.currentRoom = undefined;
-  }
+	play: function(sound, sourceClient){
+		var address = sourceClient.connection._socket.remoteAddress;
+
+		var now = new Date();
+		var allowedDate = now.getTime() - (spamTimeout * 1000);
+		
+		if (sourceClient.soundsPlayed >= maxSounds && sourceClient.lastPlayed > allowedDate) {
+			// Not allowed to play a sound
+			var remaining = Math.round((sourceClient.lastPlayed - allowedDate) / 1000);
+			console.log('You can only send ' + maxSounds + ' sounds every ' + (spamTimeout / 60) + ' minutes. (' + formatTime(remaining) + ')');
+			return;
+		} else if (sourceClient.soundsPlayed >= maxSounds) {
+			// Max sounds played, but longer than spamTimeout ago...
+			sourceClient.soundsPlayed = 0;
+		}
+
+		sourceClient.soundsPlayed++;
+		sourceClient.lastPlayed = now.getTime();
+
+		console.log("playing ", sound.name, "in room", this.uuid);
+		var soundFile = path.resolve(this.soundsFolder, sound.file);
+
+		if(this.uuid == 'server'){
+			// Play file on the server
+			if (fs.statSync(soundFile) && this.player){
+				this.player.play(soundFile, function(err){
+					if (err)
+						console.log('Couldn\'t play file "' + soundFile + '"');
+				});
+			}
+		}
+		// Send command to clients to play sound
+		this.clients.forEach(function(client){
+			if (client.uuid != sourceClient.uuid)
+				client.send(this.playSoundCommand(sound));
+		}.bind(this));
+	},
+
+	playSoundCommand: function(sound){
+		return JSON.stringify({"command": "play", "sound": sound});
+	},
+
+	addClient: function(client){
+		this.clients.push(client);
+		client.currentRoom = this;
+	},
+
+	removeClient: function(client){
+		this.clients = this.clients.filter(function(c) { return c != client });
+		client.currentRoom = undefined;
+	}
 };
 
 
--- soundboard/server/src/server.js
+++ soundboard/server/src/server.js
@@ -2,65 +2,74 @@ var uuid = require('node-uuid');
 var Room = require('./room.js');
 
 function Client(connection) {
-  this.uuid = String(uuid.v4());
-  this.connection = connection;
+	this.uuid = String(uuid.v4());
+	this.connection = connection;
+	this.lastPlayed = 0;
+	this.soundsPlayed = 0;
 }
 
 Client.prototype = {
-  send: function(msg){
-    this.connection.send(msg);
-  }
+	send: function(msg){
+		this.connection.send(msg);
+	}
 };
 
 
 var Server = {
-  rooms: {},
-  clients: {},
-  newRoom: function(client, roomId){
-    if (client.currentRoom)
-      this.leaveRoom(client, client.currentRoom);
+	rooms: {},
+	clients: {},
 
-    var room = new Room(client, roomId);
-    this.rooms[room.uuid] = room;
-    return room;
-  },
-  joinRoom: function(client, roomId){
-    var room = this.rooms[roomId];
-    if (client.currentRoom)
-      this.leaveRoom(client, client.currentRoom);
+	newRoom: function(client, roomId){
+		if (client.currentRoom)
+			this.leaveRoom(client, client.currentRoom);
 
-    if (room)
-      room.addClient(client);
-    else {
-      this.newRoom(client, roomId);
-    }
-  },
-  leaveRoom: function(client, room){
-    room.removeClient(client);
+		var room = new Room(client, roomId);
+		this.rooms[room.uuid] = room;
+		return room;
+	},
 
-    if (room.clients.length == 0){
-      this.removeRoom(room);
-    }
-  },
-  removeRoom: function(room){
-    console.log("removing room: ", room.uuid);
-    delete this.rooms[room.uuid];
-  },
-  newClient: function(connection){
-    var newClient = new Client(connection);
-    this.clients[newClient.uuid] = newClient;
-    return newClient;
-  },
-  removeClient: function(client){
-  
-    if (client.currentRoom)
-      this.leaveRoom(client, client.currentRoom);
+	joinRoom: function(client, roomId){
+		var room = this.rooms[roomId];
+		if (client.currentRoom)
+			this.leaveRoom(client, client.currentRoom);
 
-    delete this.clients[client.uuid];
-  },
-  findClient: function(clientId){
-    return this.clients[clientId];
-  }
+		if (room)
+			room.addClient(client);
+		else {
+			this.newRoom(client, roomId);
+		}
+	},
+
+	leaveRoom: function(client, room){
+		room.removeClient(client);
+
+		if (room.clients.length == 0){
+			this.removeRoom(room);
+		}
+	},
+
+	removeRoom: function(room){
+		console.log("removing room: ", room.uuid);
+		delete this.rooms[room.uuid];
+	},
+
+	newClient: function(connection){
+		var newClient = new Client(connection);
+		this.clients[newClient.uuid] = newClient;
+		return newClient;
+	},
+
+	removeClient: function(client){
+	
+		if (client.currentRoom)
+			this.leaveRoom(client, client.currentRoom);
+
+		delete this.clients[client.uuid];
+	},
+
+	findClient: function(clientId){
+		return this.clients[clientId];
+	}
 };
 
 
